c8acd9252c25b9eaec3e06c636b624d3126adccb
Sampad Adhikary <sampadadhikary031@gmail.com>
2023-09-30 12:30:22 +0530

    updated code as per manual

--- createEmp_Data.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE Emp_Data(emp_id INT, emp_name VARCHAR(50), emp_salary INT);
-INSERT INTO Emp_Data VALUES(102,'John Doe',20000);
-INSERT INTO Emp_Data VALUES(103,'Jane Doe',40000);
-INSERT INTO Emp_Data VALUES(104,'Shawn Doe',60000);
